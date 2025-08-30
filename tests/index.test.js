df6bc6ae7022955e6299fc349e79851088b3bb71
freearhey <7253922+freearhey@users.noreply.github.com>
2025-08-30 21:37:17 +0300

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,7 +7,7 @@ let data = [
     firstName: 'Jerry',
     lastName: 'Onkovich',
     age: 18,
-    passphrase: 'carpool fan',
+    passphrase: 'carpool fan+',
     url: 'http://google.com'
   },
   {
@@ -23,21 +23,22 @@ it('can find items', () => {
   let index = sj.createIndex(data)
 
   expect(index.search('jer')).toMatchObject([data[1], data[2]])
+  expect(index.search('+')).toMatchObject([data[2]])
   expect(index.search('firstName:ni')).toMatchObject([data[0]])
-  expect(index.search('firstName:^j age:>13')).toMatchObject([data[2]])
+  expect(index.search('firstName:/^j/ age:>13')).toMatchObject([data[2]])
   expect(index.search('age:>18')).toMatchObject([data[0], data[3]])
   expect(index.search('age:>13 age:<20')).toMatchObject([data[2]])
   expect(index.search('13')).toMatchObject([data[1]])
-  expect(index.search('lastName:^(k|b)')).toMatchObject([data[0], data[3]])
+  expect(index.search('lastName:/^(k|b)/')).toMatchObject([data[0], data[3]])
   expect(index.search('w y')).toMatchObject([data[3]])
   expect(index.search('ch,kh')).toMatchObject([data[0], data[2]])
   expect(index.search('car p')).toMatchObject([data[0], data[2]])
   expect(index.search('"car p"')).toMatchObject([data[0]])
   expect(index.search('http://test.com?hash=abc')).toMatchObject([data[3]])
   expect(index.search('url:http://test.com?hash=abc')).toMatchObject([data[3]])
-  expect(index.search('firstName:^n')).toMatchObject([data[0]])
-  expect(index.search('url:^$')).toMatchObject([data[1]])
-  expect(index.search('url:.')).toMatchObject([data[2], data[3]])
+  expect(index.search('firstName:/^n/')).toMatchObject([data[0]])
+  expect(index.search('url:/^$/')).toMatchObject([data[1]])
+  expect(index.search('url:/./')).toMatchObject([data[2], data[3]])
 })
 
 it('can find items when options.searchable is specified', () => {
